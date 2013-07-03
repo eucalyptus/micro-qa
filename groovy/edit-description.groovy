f83aba149557b06595c3766a11cbd214d9f3c682
Vic Iglesias <viglesiasce@gmail.com>
2013-07-02 23:29:02 -0700

    Include a property in the description

--- groovy/edit-description.groovy
+++ groovy/edit-description.groovy
@@ -1,12 +1,15 @@
-def str_view = "QA Maint"
+def str_view = "QA Mainline"
 def view = Hudson.instance.getView(str_view)
  
 //copy all projects of a view
 for(item in view.getItems())
-{
-
-  //create the new project name
-  item.description = '<a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastBuild/downstreambuildview/?">Latest Result</a><br><a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastFailedBuild/downstreambuildview/?">Latest Failed</a><br><a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastStableBuild/downstreambuildview/?">Latest Stable</a><br>'
-  print item.description    
-
+{ 
+  //Get all properties of the project
+  prop = item.getProperty(ParametersDefinitionProperty.class)
+  item.description = '<a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastBuild/downstreambuildview/?">Latest Result</a><br>'
+  item.description += '<a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastFailedBuild/downstreambuildview/?">Latest Failed</a><br>'
+  item.description += '<a href="http://jenkins.qa1.eucalyptus-systems.com/job/' + item.getName() + '/lastStableBuild/downstreambuildview/?">Latest Stable</a><br>'
+  item.description += '<a href="http://jenkins.qa1.eucalyptus-systems.com/job/qa-free-machines/buildWithParameters?test_name=' + prop.getParameterDefinition("test_name").getDefaultParameterValue().value + '">Free Machines</a><br>'
+    
+  println item.description    
 }
