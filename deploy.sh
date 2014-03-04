40146b7b24a887cb67f8d575a5a33c3df4d3fc73
Vic Iglesias <viglesiasce@gmail.com>
2014-03-03 18:14:37 -0800

    Checkout 0.1.5

--- deploy.sh
+++ deploy.sh
@@ -129,7 +129,11 @@ knife cookbook site install eucalyptus
 knife cookbook upload eucalyptus
 
 ## Upload roles
+git clone https://github.com/eucalyptus/eucalyptus-cookbook
+pushd eucalyptus-cookbook
+git checkout 0.1.5
 knife role from file eucalyptus/roles/*
+popd
 popd
 
 ### Create rc.local
