c60762dfcc435c1fea74a5816d7e9cdbb699b3a7
Vic Iglesias <viglesiasce@gmail.com>
2014-04-15 13:43:23 -0700

    Fix selenium install

--- deploy.sh
+++ deploy.sh
@@ -74,11 +74,10 @@ popd
 easy_install selenium
 yum -y install make rubygems ruby-devel xorg-x11-font* wget xorg-x11-server-Xvfb firefox
 cd /
-wget -q https://selenium.googlecode.com/files/selenium-server-2.35.0.zip
-unzip selenium-server-2.35.0.zip
+wget -q https://selenium.googlecode.com/files/selenium-server-2.39.0.zip
+unzip selenium-server-2.39.0.zip
 dbus-uuidgen > /var/lib/dbus/machine-id
 Xvfb :0 -ac 2> /dev/null &
-export DISPLAY=":0" && nohup java -jar selenium-2.35.0/selenium-server-standalone-2.35.0.jar -trustAllSSLCertificates &
 
 ### Install chef dependencies
 curl -L https://www.opscode.com/chef/install.sh | bash
